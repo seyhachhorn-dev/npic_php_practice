4ce778c228cdefc2c145a055e8d1ebdc6d6ec8d4
CHHORN Seyha <senjihachiro@gmail.com>
2026-01-25 20:35:42 +0700

    implemented auth functions

--- init/func/auth.func.init.php
+++ init/func/auth.func.init.php
@@ -5,6 +5,7 @@ function registerUser ($name, $username, $password){
 
     global $db;
     isUsernameExists($username);
+    
     $query = $db->prepare("INSERT INTO tbl_users(name, username, password) VALUES (?, ?, ?)");
     $query -> bind_param("sss",$name, $username, $password);
     $query -> execute();
@@ -15,6 +16,49 @@ function registerUser ($name, $username, $password){
         return false;
     }
 }
+
+// login
+
+function loginUser ($username, $password){
+    global $db;
+    $query = $db->prepare("SELECT * FROM tbl_uers where username = ? AND password = ?");
+    $query -> bind_param("ss",$username, $password);
+    $query -> execute();
+    $rs = $query -> get_result();
+
+    if($rs -> num_rows){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function deleteUser ($userID){
+    global $db;
+    
+    $query = $db->prepare("DELETE FROM tbl_users where user_id = ?");
+    $query -> bind_param("i",$userID);
+    $query -> execute();
+    if($query -> affected_rows >0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateUer ($userID, $name, $username,$password){
+    global $db;
+
+    $query = $db->prepare("UPDATE tbl_users SET name = ?, username = ?, password = ? WHERE user_id = ?");
+    $query -> bind_param("sssi",$name, $username, $password, $userID);
+    $query -> execute();
+    if($query -> affected_rows >0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // validate useranme
 
 function isUsernameExists($username){
