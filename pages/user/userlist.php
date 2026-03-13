908d254f296e0b8b1f372092790164ddc500db7a
CHHORN Seyha <senjihachiro@gmail.com>
2026-03-13 11:14:27 +0700

    user list impl

--- pages/user/userlist.php
+++ pages/user/userlist.php
@@ -1,2 +1,6 @@
-<h1>User List</h1>
-<a href="<?php echo $baseUrl ?>?page=user/createuser">Create User</a>
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h1>User List</h1>
+        <a href="<?php echo $baseUrl ?>?page=user/createuser" class="btn btn-success">Create User</a>
+    </div>
+</div>
